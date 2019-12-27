20faf74879416117f77ab941e6a6d9ec51b6840d
Lysia Stolhofer <lysiapaulinstolhofer@gmail.com>
2019-12-27 11:37:38 -0500

    files created

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <div className="footer text-center">Copyright © Lysia Stolhofer 2019</div>
+);
+
+export default Footer;
